cab2608f775c6238515a4e936467f218e022194e
Firolilo <brunofioriloar@gmail.com>
2024-10-28 08:30:56 -0400

    weu

---
+++ Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Juan.Data;
+using Juan.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Juan.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private MyDBContext _dbc;
+        public UserController(MyDBContext dbc)
+        {
+            _dbc = dbc;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Listar()
+        {
+            return await _dbc.Usuarios.ToListAsync();
+        }
+    }
+}
---
+++ Data/MyDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Juan.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Juan.Data
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Usuarios { get; set; }
+    }
+}
